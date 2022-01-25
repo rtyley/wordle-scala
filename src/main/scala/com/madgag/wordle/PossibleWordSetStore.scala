37efa42ba6c0e6f63ddae301734d25825ee57697
Roberto Tyley <roberto.tyley@gmail.com>
2022-01-25 21:34:04 +0000

    Maybe we *should* intern, but for a new data-type

--- src/main/scala/com/madgag/wordle/PossibleWordSetStore.scala
+++ src/main/scala/com/madgag/wordle/PossibleWordSetStore.scala
@@ -34,6 +34,25 @@ object PossibleWordSetStore {
   }
 }
 
+/*
+Why not interning? Well, comparing two BitSets for equality is really slow. Can't use them as keys for Maps easily,
+so can't do things like caching the results of intersecting two sets to get a new set.
+
+What if we were interning to a better format, one that was, yes, more compact than a BitSet for our sparse data
+BUT ALSO carried INSIDE IT a unique identifier that could be used for instant equality checks?
+
+What if you could compare equality by looking at a single long, rather than all the data? What if the first,
+and possibly only, entry in the array-of-longs was the id (allocated from an AtomicInteger for WordSets that
+have more than 5 items)?
+
+When Wordset has 6 items - we *must* be able to compare equality JUST by looking at the first entry (and possibly array length?!?).
+
+
+Or what if we simply use BitSet, but store it in a java.util.IdentityHashMap? How would that work for
+caching the results of intersecting two sets to get a new set??
+
+
+*/
 //object WordSetInterning {
 //
 //  val m: TrieMap[Set[Int], BitSet] = TrieMap.empty

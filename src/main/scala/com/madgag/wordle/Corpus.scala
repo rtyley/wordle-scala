56cc73ab9f551608de2007f289633323b100c6ef
Roberto Tyley <roberto.tyley@gmail.com>
2022-01-28 14:44:33 +0000

    Can update a candidates list if presented with evidence

--- src/main/scala/com/madgag/wordle/Corpus.scala
+++ src/main/scala/com/madgag/wordle/Corpus.scala
@@ -3,6 +3,7 @@ package com.madgag.wordle
 import com.google.common.io.{CharStreams, Resources}
 import com.madgag.wordle.WordFeedback.feedbackFor
 import com.madgag.wordle.Wordle.Word
+import com.madgag.wordle.approaches.tartan.Candidates
 
 import java.io.{File, FileInputStream, FileOutputStream, ObjectInputStream, ObjectOutputStream}
 import java.nio.charset.StandardCharsets.UTF_8
@@ -26,12 +27,37 @@ case class Corpus(commonWords: SortedSet[Word], uncommonWords: SortedSet[Word]) 
 
   // def humanReadableWordsFor(bitMap: BitSet): Set[Word] = bitMap.map(orderedCommonWords(_))
 
+  val initialCandidates: Candidates = Candidates(
+    possibleWords = SortedSet.from(0 until commonWords.size),
+    discriminators = SortedSet.from(commonWords.size until allWordsOrdered.size)
+  )
+
+  def updateCandidatesWithEvidence(candidates: Candidates, evidenceWordId: Int, evidenceFeedback: WordFeedback): Candidates = {
+    val gridEntryForEvidenceWord = grid(evidenceWordId)
+    val (possibleWordsThatRemainPossible, possibleWordsThatBecameImpossible) =
+      candidates.possibleWords.partition(gridEntryForEvidenceWord(_) == evidenceFeedback.underlying)
+
+    // if the `Candidates` for possibleWordsThatRemainPossible is already available, return it, skip next part
+
+    val updatedDiscriminators: SortedSet[Int] = (candidates.discriminators ++ possibleWordsThatBecameImpossible).filter { wordId =>
+        val gridEntryForWord = grid(wordId)
+        val firstFeedback = gridEntryForWord(possibleWordsThatRemainPossible.head)
+        possibleWordsThatRemainPossible.tail.exists(gridEntryForWord(_) != firstFeedback)
+      }
+
+    Candidates(
+      possibleWords = possibleWordsThatRemainPossible,
+      discriminators = updatedDiscriminators
+    )
+  }
+
+
   lazy val grid: Array[Array[Byte]] = {
     println(gridStorage.getAbsolutePath)
     if (gridStorage.exists()) {
-      Using(new ObjectInputStream(new GZIPInputStream(new FileInputStream(gridStorage)))) { s =>
+      Using.resource(new ObjectInputStream(new GZIPInputStream(new FileInputStream(gridStorage)))) { s =>
         s.readObject().asInstanceOf[Array[Array[Byte]]]
-      }.get
+      }
     } else {
       val tmpFile = File.createTempFile("temp", "skunky")
       val gd = for {
@@ -41,7 +67,7 @@ case class Corpus(commonWords: SortedSet[Word], uncommonWords: SortedSet[Word]) 
         for (targetWord <- commonWordsOrdered) yield feedbackFor(candidateWord, targetWord).underlying
       }.toArray
 
-      Using(new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream(tmpFile)))) { s =>
+      Using.resource(new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream(tmpFile)))) { s =>
         s.writeObject(gd)
       }
 

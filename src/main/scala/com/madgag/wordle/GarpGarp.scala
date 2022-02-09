24045aafd3979d8cf6075c795409f805f40bff1a
Roberto Tyley <roberto.tyley@gmail.com>
2022-02-09 17:32:52 +0000

    This is probably a good idea - cache at PossCanSetsIfCanPlayed.findCandidateScoringBetterThan ??

--- src/main/scala/com/madgag/wordle/GarpGarp.scala
+++ src/main/scala/com/madgag/wordle/GarpGarp.scala
@@ -13,44 +13,67 @@ case class WordGuessSum(wordId: WordId, guessSum: Int) extends Ordered[WordGuess
   def addGuesses(x: Int) = copy(guessSum = guessSum + x)
 }
 
+case class FParams(guessIndex: Int, h: Candidates)
+case class FResult(beta: Int, wordGuessSum: WordGuessSum)
 
 
 class GarpGarp(
   analysisForCorpusWithGameMode: AnalysisForCorpusWithGameMode
 ) {
+  case class PossCanSetsIfCanPlayed(t: WordId, possibleCandidates: Seq[Candidates]) {
+    val fastCandidatesSetHash: Int = possibleCandidates.hashCode // we rely on the hashcode a lot for `Set`s, so compute once...!
+
+    val partitionEvennessScore: Int = possibleCandidates.map(c => c.possibleWords.size * c.possibleWords.size).sum
+
+    def findCandidateScoringBetterThan(thresholdToBeat: Int, nextGuessIndex: Int): Option[WordGuessSum] = possibleCandidates.foldM(0) {
+      case (acc, candidates) if thresholdToBeat > acc =>
+        Some(acc + f(nextGuessIndex, candidates, thresholdToBeat - acc).guessSum)
+      case _ => None
+    }.filter(_ < thresholdToBeat).map {
+      newBestScore => WordGuessSum(t, newBestScore)
+    }
+  }
 
   val newBestScoreCounter = new LongAdder()
   val callsToFCounter = new LongAdder()
-  
-  // Change to return Option?
-  def f(guessIndex: Int, h: Candidates, beta: Int = 1000000): WordGuessSum = if (guessIndex>=6) WordGuessSum(-1,0) else {
+
+  val fResultsByFParams: java.util.concurrent.ConcurrentMap[FParams,FResult] =
+    new java.util.concurrent.ConcurrentHashMap()
+
+  /**
+   *
+   * @param beta only pursue results that are better (lower) than this threshold - results that >= to this threshold
+   *             are useless.
+   * @return accurate result, if one could be found below the beta threshold
+   */
+  def f(guessIndex: Int, h: Candidates, beta: Int = 1000000): WordGuessSum = if (guessIndex>=6) WordGuessSum(-1,1000000) else {
     callsToFCounter.increment()
     val numPossibleWords = h.possibleWords.size
     numPossibleWords match {
-      case 0 => WordGuessSum(-1,0)
+      case 0 => throw new IllegalStateException("Can't be!")
       case 1 => WordGuessSum(h.possibleWords.head,1)
       case 2 => WordGuessSum(h.possibleWords.head,3)
       case _ => {
-        val candidatesWithPartitionMostPromisingFirst: Seq[PossCanSetsIfCanPlayed] = h.allWords.toSeq.map { t =>
-          possibleCandidateSetsIfCandidatePlayed(h, t)
-        }.distinctBy(_.fastCandidatesSetHash).sortBy(_.partitionEvennessScore)
+        // Hit cache for FParams
+        // If miss - calculate the stuff, store it
+        // if hit - can we use the calculated data? YES if:
+        // OLD-beta > NEW-beta --
+        //   ie, we searched really hard, and permitted some really bad answers which you should now feel free to discard,
+        //
+        // or... old-result < NEW-beta
+        // typically old-result (if found) < OLD-beta
+        // if OLD-beta < NEW-beta, that doesn't mean that we can't use old-result, so long as it's < NEW-beta
+        // if old-result > NEW-beta (a failed-to-find solution), surely we can still use old-result so long as OLD-beta > NEW-beta
 
+        val fParams = FParams(guessIndex, h)
         val nextGuessIndex = guessIndex + 1
-        candidatesWithPartitionMostPromisingFirst.foldLeft(WordGuessSum(-1, beta)) { case (bestSoFar, possCanSetsIfCanPlayed) =>
-          possCanSetsIfCanPlayed.possibleCandidates.toSeq.foldM(0) {
-            case (acc, candidates) if bestSoFar.guessSum > acc => {
-              Some(acc + f(nextGuessIndex, candidates, bestSoFar.guessSum - acc).guessSum)
-            }
-            case _ => None
-          }.filter(_ < bestSoFar.guessSum).map {
-            newBestScore =>
-              if (bestSoFar.wordId != -1) {
-                newBestScoreCounter.increment()
-              }
 
-              WordGuessSum(possCanSetsIfCanPlayed.t, newBestScore)
-          }.getOrElse(bestSoFar)
-        }.addGuesses(numPossibleWords)
+        h.allWords.toSeq.map { t =>
+          possibleCandidateSetsIfCandidatePlayed(h, t)
+        }.distinctBy(_.fastCandidatesSetHash).sortBy(_.partitionEvennessScore).foldLeft(WordGuessSum(-1, beta)) {
+          case (bestSoFar, possCanSetsIfCanPlayed) =>
+            possCanSetsIfCanPlayed.findCandidateScoringBetterThan(bestSoFar.guessSum, nextGuessIndex).getOrElse(bestSoFar)
+        }.addGuesses(h.possibleWords.size)
       }
     }
   }
@@ -75,8 +98,3 @@ class GarpGarp(
   }
 }
 
-case class PossCanSetsIfCanPlayed(t: WordId, possibleCandidates: Seq[Candidates]) {
-  val fastCandidatesSetHash: Int = possibleCandidates.hashCode // we rely on the hashcode a lot for `Set`s, so compute once...!
-
-  val partitionEvennessScore: Int = possibleCandidates.map(c => c.possibleWords.size * c.possibleWords.size).sum
-}

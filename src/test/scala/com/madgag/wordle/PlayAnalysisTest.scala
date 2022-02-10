986285903edc9140f1641db3e835bcdc1418f26a
Roberto Tyley <roberto.tyley@guardian.co.uk>
2022-02-10 23:17:38 +0000

    Go large

--- src/test/scala/com/madgag/wordle/PlayAnalysisTest.scala
+++ src/test/scala/com/madgag/wordle/PlayAnalysisTest.scala
@@ -32,7 +32,7 @@ class PlayAnalysisTest extends AnyFlatSpec with Matchers {
   }
 
   it should "give the correct answer, for a sub-half-minute perf check" in {
-    given Corpus = Full.reducedByAFactorOf(80)
+    given Corpus = Full.reducedByAFactorOf(10)
 
     val playAnalysis = forGameMode(Normal)
     val best = playAnalysis.bestInitial
